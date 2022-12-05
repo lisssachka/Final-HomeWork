4b78306bcf01b59bc6b3bb016ff9e61722096b6e
lisssachka <alisek@mail.ru>
2022-12-05 18:19:38 +0400

    Added CreateArray

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿string[] CreateArray (int size)
+{
+    string[] array = new string[size];
+    for (int i = 0; i < size; i++)
+    {
+        Console.WriteLine($"Введите {i+1}-й элемент: ");
+        string element = Convert.ToString(Console.ReadLine());
+        array[i] = element;
+    }
+    return array;
+}
+
+
